1776e7b35e1474e577b69a031b6e7c228b5f747f
ojlucas <ojunianlucas@gmail.com>
2023-05-05 18:13:01 -0300

    noticias actualizadas

--- js/javascript.js
+++ js/javascript.js
@@ -40,24 +40,32 @@ function mostrarNotas(laclase){
 
   
 function cambiarslide(x){ 
+    document.getElementById('slide').classList.remove("slide-in-top");
+    document.getElementById('slide').offsetWidth;
+    document.getElementById('slide').classList.add("slide-in-top"); 
     document.getElementById('slide').style.background="url(" + elslide.destacadas[x].imagen + ") no-repeat";      
     document.getElementById('slide').style.backgroundSize="cover";      
     document.getElementById('slide').style.backgroundPositionY="-200px";      
     document.getElementById('slide').onclick = function(){ window.location=elslide.destacadas[x].link; } ; 
     document.getElementById('tituloslide').innerHTML="<a href='"+ elslide.destacadas[x].link  + "'>" + elslide.destacadas[x].titulo + "</a>";      
+    document.getElementById('tituloslide').classList.remove("slide-in-left");
+    document.getElementById('tituloslide').classList.remove("delay05");
+    document.getElementById('tituloslide').offsetWidth;
+    document.getElementById('tituloslide').classList.add("slide-in-left"); 
+    document.getElementById('tituloslide').classList.add("delay05"); 
     //console.log('imagen ' + elslide.destacadas[x].imagen);   
 }
  
   var datosslide = '{ "destacadas" : [' +
-'{ "imagen":"./imagenes/fotoslide1.jpg" , "titulo":"Titulo nota 1", "link": "nota1.html" },' +
-'{ "imagen":"./imagenes/fotoslide2.jpg" , "titulo":"Titulo nota 2", "link": "nota2.html" },' +
-'{ "imagen":"./imagenes/fotoslide3.jpg" , "titulo":"Titulo nota 3", "link": "nota3.html" } ]}';
+'{ "imagen":"./imagenes/fotoslide1.jpg" , "titulo":"La Universidad de la Plata conquistar&aacute; el espacio con su propio nanosat&eacute;lite", "link": "nota1.html" },' +
+'{ "imagen":"./imagenes/fotoslide2.jpg" , "titulo":"ESTA HERRAMIENTA ARGENTINA CONVIERTE TRANSCRIBE LOS AUDIOS EN TEXTO", "link": "nota2.html" },' +
+'{ "imagen":"./imagenes/fotoslide3.jpg" , "titulo":"¡Por fin! WhatsApp es ahora multidispositivo", "link": "nota3.html" } ]}';
   const elslide = JSON.parse(datosslide);
   var x=0;
 setInterval(() => {   
     x++;
     if (x>=elslide.destacadas.length) x = 0;
     cambiarslide(x);  
-  }, 4000);
+  }, 4500);
 
   cambiarslide(x);
