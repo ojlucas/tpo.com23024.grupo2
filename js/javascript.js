705a7001552cd5f7f630e7b49de61dbc1b7a5a2f
ojlucas <ojunianlucas@gmail.com>
2023-05-05 15:57:21 -0300

    slide con link

--- js/javascript.js
+++ js/javascript.js
@@ -39,21 +39,25 @@ function mostrarNotas(laclase){
 
 
   
- 
-  var datosslide = '{ "destacadas" : [' +
-'{ "imagen":"./imagenes/fotoslide1.jpg" , "titulo":"Titulo nota 1" },' +
-'{ "imagen":"./imagenes/fotoslide2.jpg" , "titulo":"Titulo nota 2" },' +
-'{ "imagen":"./imagenes/fotoslide3.jpg" , "titulo":"Titulo nota 3" } ]}';
- 
-  const elslide = JSON.parse(datosslide);
-  var x=0;
-setInterval(() => {
-    if (x>=elslide.destacadas.length) x = 0;
+function cambiarslide(x){ 
     document.getElementById('slide').style.background="url(" + elslide.destacadas[x].imagen + ") no-repeat";      
     document.getElementById('slide').style.backgroundSize="cover";      
     document.getElementById('slide').style.backgroundPositionY="-200px";      
-    document.getElementById('tituloslide').innerHTML=elslide.destacadas[x].titulo;      
-    console.log('imagen ' + elslide.destacadas[x].imagen + ' ' +elslide.destacadas.length);
+    document.getElementById('slide').onclick = function(){ window.location=elslide.destacadas[x].link; } ; 
+    document.getElementById('tituloslide').innerHTML="<a href='"+ elslide.destacadas[x].link  + "'>" + elslide.destacadas[x].titulo + "</a>";      
+    //console.log('imagen ' + elslide.destacadas[x].imagen);   
+}
+ 
+  var datosslide = '{ "destacadas" : [' +
+'{ "imagen":"./imagenes/fotoslide1.jpg" , "titulo":"Titulo nota 1", "link": "nota1.html" },' +
+'{ "imagen":"./imagenes/fotoslide2.jpg" , "titulo":"Titulo nota 2", "link": "nota2.html" },' +
+'{ "imagen":"./imagenes/fotoslide3.jpg" , "titulo":"Titulo nota 3", "link": "nota3.html" } ]}';
+  const elslide = JSON.parse(datosslide);
+  var x=0;
+setInterval(() => {   
     x++;
+    if (x>=elslide.destacadas.length) x = 0;
+    cambiarslide(x);  
   }, 4000);
 
+  cambiarslide(x);
