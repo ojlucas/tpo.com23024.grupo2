2de178033baf29246464c4567516153c7c7bd384
ojlucas <ojunianlucas@gmail.com>
2023-05-09 18:54:08 -0300

    leer api con async/await

--- js/consumirapi.js
+++ js/consumirapi.js
@@ -1,40 +1,47 @@
    
- /**https://github.com/cyberboysumanjay/Inshorts-News-API 
-      //content
-      //imageUrl
-      //readMoreUrl
-      //url 
+ /*
+ API PARA NOTICIAS
+ https://github.com/cyberboysumanjay/Inshorts-News-API 
+ title
+ content
+ imageUrl
+ readMoreUrl
+ url 
  */
+
+ const LeerApi = async () => {
+  try{
+    const res = await fetch("https://inshorts.deta.dev/news?category=technology");
+    const datos = await res.json();
+    console.log( ' respuesta noticias ');
+    return datos;
+  } catch(err) {
+    console.log('Error LeerApi: ',err);
+    return '';
+  }
+};
  
  function consultarNoticias() {
 
-    let contenedor = document.getElementById("listadonovedades");
-    contenedor.innerHTML = '<div align="center"><div class="lds-ring"><div></div><div></div><div></div><div></div></div><br>Cargando noticias externas, puede tardar unos segundos...</div> ';
-    
-fetch('https://inshorts.deta.dev/news?category=technology')
-  .then(response => {
-    if (response.ok)
-      return response.text();
-    else
-      throw new Error(response.status);
-  })
-  .then(data => {
-    console.log( ' respuesta noticias ');
-    const resultadoapi = JSON.parse(data);
+  let contenedor = document.getElementById("listadonovedades");
+  contenedor.innerHTML = '<div align="center"><div class="lds-ring"><div></div><div></div><div></div><div></div></div><br>Cargando noticias externas, puede tardar unos segundos...</div> ';
+  
+  console.log( ' cargando noticias ');
+  let datos = LeerApi().then(resultadoapi => {   
     contenedor.innerHTML = '';
-    document.getElementById("titulo").innerHTML = resultadoapi.data.length +' NOTICIAS INTERNACIONALES';
-    for (x=0;x<=resultadoapi.data.length;x++){      
+    document.getElementById("titulo").innerHTML = resultadoapi.data.length +' NOTICIAS INTERNACIONALES';    
+    for(let dato of resultadoapi.data){    
       contenedor.innerHTML += '<div class="centroTitularunaNoticia aparecer " ><div class="miniaturaimagenNota">' +
-        '<a href="' + resultadoapi.data[x].readMoreUrl + '" target="_blank"><img src="'+ resultadoapi.data[x].imageUrl + '"></a></div>' +
-        '<div class="tituloNota">' + resultadoapi.data[x].title + '</div>' +
-        '<div class="resumenNota">' + resultadoapi.data[x].content.substr(0, 180) + '...</div>' +
-        '<div class="leermas"><a href="' + resultadoapi.data[x].readMoreUrl + '" target="_blank">Leer m&aacute;s</a></div>' + 
-    '</div>';
+        '<a href="' + dato.readMoreUrl + '" target="_blank"><img src="'+ dato.imageUrl + '"></a></div>' +
+        '<div class="tituloNota">' + dato.title + '</div>' +
+        '<div class="resumenNota">' + dato.content.substr(0, 180) + '...</div>' +
+        '<div class="leermas"><a href="' + dato.readMoreUrl + '" target="_blank">Leer m&aacute;s</a></div>' + 
+        '</div>';
     }
-  })
-  .catch(err => {
-    console.error("ERROR: ", err.message)    
-  }); 
+
+  }).catch(err =>  console.error("Error: " + err.message));
+ 
+      
 }
-console.log( ' cargando noticias ');
+
 consultarNoticias();
